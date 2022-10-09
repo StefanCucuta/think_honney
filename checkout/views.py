78aefb37f5e4736e24a3c3debb4a009f346bde85
Stefan Cucuta <cucutastefan@gmail.com>
2022-10-09 17:24:35 +0000

    added stripe elements / removed the key in order not to go in to gitpod

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Lg9gxDJzfiGgNdCotREiegyVYpvNlEjc7zqTsL7KFzIF8whAZzfA1hTuSMJEGJzNeNfv2Do4M8NcLED1TYAj86u00Hfgat0Z8',
+        'client-secret': 'test client secret',
     }
 
     return render(request, template, context)
