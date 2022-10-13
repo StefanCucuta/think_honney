08d42ee0a9479f65d898c667bfe5e8386a087390
Stefan Cucuta <cucutastefan@gmail.com>
2022-10-13 16:07:37 +0000

    Deloy to heroku 2

--- think_honney/settings.py
+++ think_honney/settings.py
@@ -163,7 +163,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-eu'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
